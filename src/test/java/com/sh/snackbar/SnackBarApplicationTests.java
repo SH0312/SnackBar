4629f67a98d21dbe7d749cb11e65a0fad0db4433
sh <SH0312>
2022-09-14 00:07:41 +0900

    first commit

---
+++ src/test/java/com/sh/snackbar/SnackBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.snackbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
